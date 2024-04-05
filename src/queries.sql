2b861d4753af06a872375803f2a21458d9e9c880
Ümit Can Evleksiz <evleksizumitcan@gmail.com>
2024-04-05 18:26:28 +0300

    add more queries

--- src/queries.sql
+++ src/queries.sql
@@ -101,15 +101,17 @@ USE VolleyDB;
 -- INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('g_orge', 'Go.1993', 'Gizem ', 'Örge', '26/04/1993', '170', '59');
 
 
--- Query 1
--- Find the number of players and display this as player count. The required column names are respectively: player count.;
+-- ✅ Query 1
+-- Find the number of players and display this as player count. The required column names are respectively: player count.
+;
 SELECT
 	COUNT(*) AS player_count
 FROM
 	Player;
 
--- Query 2
--- List the match sessions which are played before 2024 (exclusive). Display date in DD/MM/YYYY format. The required column names are respectively: session_ID, assigned_jury_username, rating, date. Sort the results by date in ascending order.;
+-- ✅ Query 2
+-- List the match sessions which are played before 2024 (exclusive). Display date in DD/MM/YYYY format. The required column names are respectively: session_ID, assigned_jury_username, rating, date. Sort the results by date in ascending order.
+;
 SELECT
 	session_ID,
 	assigned_jury_username,
@@ -123,8 +125,9 @@ ORDER BY
 	STR_TO_DATE(`date`, '%d.%m.%Y') ASC;
 
 
--- Query 3
--- List all the fields of the match sessions with the minimum rating. Display Date in DD.MM.YYYY format. The required column names are respectively: session_ID, team_ID, stadium_ID, stadium name, stadium country, time slot, date, assigned jury username, rating.;
+-- ✅ Query 3
+-- List all the fields of the match sessions with the minimum rating. Display Date in DD.MM.YYYY format. The required column names are respectively: session_ID, team_ID, stadium_ID, stadium_name, stadium_country, time_slot, date, assigned_jury_username, rating.
+;
 SELECT
 	*
 FROM
@@ -136,8 +139,9 @@ WHERE
 		FROM
 			MatchSession);
 
--- Query 4
--- List assigned jury username and stadium name of the match sessions with the maximum rating. The required column names are respectively: assigned jury username, stadium name. Sort the results by assigned jury username in descending order.;
+-- ✅ Query 4
+-- List assigned jury username and stadium name of the match sessions with the maximum rating. The required column names are respectively: assigned jury username, stadium name. Sort the results by assigned jury username in descending order.
+;
 SELECT
 	assigned_jury_username,
 	stadium_name
@@ -152,16 +156,18 @@ WHERE
 	ORDER BY
 		assigned_jury_username DESC;
 
--- Query 5
--- Find the average rating of all match sessions. The required column names are respectively: average rating.;
+-- ✅ Query 5
+-- Find the average rating of all match sessions. The required column names are respectively: average rating.
+;
 SELECT
 	AVG(rating) AS average_rating
 FROM
 	MatchSession;
 
 
--- Query 6
--- List all teams together with the number of players in each Team. Notice that in the Team table, each team id is unique. However, different ids may have the same team names as different agreements with coaches in the past were needed to be recorded. Note that there may be empty teams. The required column names are respectively: team name, coach name, coach surname, player count.;
+-- ✅ Query 6
+-- List all teams together with the number of players in each Team. Notice that in the Team table, each team id is unique. However, different ids may have the same team names as different agreements with coaches in the past were needed to be recorded. Note that there may be empty teams. The required column names are respectively: team name, coach name, coach surname, player count.
+;
 SELECT
 	T.team_name,
 	C.name AS coach_name,
@@ -179,12 +185,14 @@ GROUP BY
 ORDER BY
 	player_count DESC;
 
--- Query 7
--- Insert a new position, whose position ID is “5” and position name is “Middle Hitter”. No required column names. No required output.;
-INSERT INTO `position` (position_ID, position_name)  VALUES (5, 'Middle Hitter');
+-- ✅ Query 7
+-- Insert a new position, whose position ID is “5” and position name is “Middle Hitter”. No required column names. No required output.
+;
+INSERT INTO `Position` VALUES (5, 'Middle Hitter');
 
--- Query 8
--- Find the names and surnames of coaches who have directed more than or equal to 2 match sessions. The required column names are respectively: name, sur- name. Sort by surname in descending order.;
+-- ✅ Query 8
+-- Find the names and surnames of coaches who have directed more than or equal to 2 match sessions. The required column names are respectively: name, sur- name. Sort by surname in descending order.
+;
 SELECT
 	C.name,
 	C.surname
@@ -199,8 +207,9 @@ HAVING
 ORDER BY
 	C.surname DESC;
 
--- Query 9
--- Find the names and surnames of players who were born in the same year as “Ebrar Karakurt” and who are taller than her. The required column names are respectively: name, surname. Sort by surname in ascending order.;
+-- ✅ Query 9
+-- Find the names and surnames of players who were born in the same year as “Ebrar Karakurt” and who are taller than her. The required column names are respectively: name, surname. Sort by surname in ascending order.
+;
 SELECT
 	`name`,
 	surname
@@ -217,8 +226,9 @@ WHERE
 			AND surname = 'Karakurt')
 	AND (`name` != 'Ebrar ' OR `surname` != 'Karakurt');
 
--- Query 10
--- List all the coaches’ names who directed a single team non-stop between 02.09.2024- 31.12.2025 (inclusive) and that team has agreement with “Digiturk”. The required column names are respectively: name, surname, channel name, contract start, contract finish. Sort by name in ascending order.;
+-- ✅ Query 10
+-- List all the coaches’ names who directed a single team non-stop between 02.09.2024- 31.12.2025 (inclusive) and that team has agreement with “Digiturk”. The required column names are respectively: name, surname, channel name, contract start, contract finish. Sort by name in ascending order.
+;
 SELECT
 	C.name,
 	C.surname,
@@ -233,8 +243,9 @@ WHERE
 	AND STR_TO_DATE(T.contract_finish, '%d.%m.%Y') >= date('2025-12-31')
 	AND T.channel_name = 'Digiturk';
 
--- Query 11
--- List all match sessions that were not directed by Daniele Santarelli and were not played in UK. The required column names are respectively: session ID, name, surname, stadium name, stadium country, team name. Sort by session id in ascending order.;
+-- ✅ Query 11
+-- List all match sessions that were not directed by Daniele Santarelli and were not played in UK. The required column names are respectively: session ID, name, surname, stadium name, stadium country, team name. Sort by session id in ascending order.
+;
 SELECT M.session_ID, C.`name`, C.surname, M.stadium_name, T.team_name
 FROM MatchSession M
 LEFT JOIN Team T ON M.team_ID = T.team_ID 
@@ -245,31 +256,146 @@ ORDER BY
 	M.session_ID;
 
 -- Query 12
--- For each year, find and list the coach who has directed the team with the highest average rating in that year. The required column names are respectively: name, surname, year, average rating. Sort by year in ascending order.;
+-- For each year, find and list the coach who has directed the team with the highest average rating in that year. The required column names are respectively: name, surname, year, average rating. Sort by year in ascending order.
+;
+SELECT
+	C.name,
+	C.surname,
+	YEAR(M.date) AS year,
+	MAX(AVG(M.rating)) OVER (PARTITION BY YEAR(M.date)) AS average_rating
+FROM
+	MatchSession M
+	INNER JOIN Team T ON M.team_ID = T.team_ID
+	INNER JOIN Coach C ON T.coach_username = C.username
+GROUP BY
+	YEAR(M.date),
+	T.team_ID
+ORDER BY
+	year ASC;
 
--- Query 13
--- For each stadium, find the coach who has directed the highest number of matches in that stadium. Please note that there can be multiple maximums. You can check the provided result table. The required column names are re- spectively: stadium name, name, surname, directed count.;
+-- ✅ Query 13
+-- For each stadium, find the coach who has directed the highest number of matches in that stadium. Please note that there can be multiple maximums. You can check the provided result table. The required column names are respectively: stadium name, name, surname, directed count.
+;
 
--- Query 14
--- List all the matches that were played after 2023 (meaning starting from 01.01.2024 inclusive), and have not been directed by “Ferhat Akba ̧s”. The required column names are respectively: session ID, name, surname. Sort by session id in ascending order.;
+SELECT
+	MS.stadium_name,
+	C.name,
+	C.surname,
+	COUNT(*) AS directed_count
+FROM
+	MatchSession MS
+	JOIN Team T ON T.team_ID = MS.team_ID
+	JOIN Coach C ON C.username = T.coach_username
+GROUP BY
+	MS.stadium_name,
+	C.name,
+	C.surname
+HAVING
+	COUNT(*) = (
+		SELECT
+			MAX(directed_count)
+		FROM (
+			SELECT
+				COUNT(*) AS directed_count
+			FROM
+				MatchSession MS2
+				JOIN Team T2 ON T2.team_ID = MS2.team_ID
+				JOIN Coach C2 ON C2.username = T2.coach_username
+			WHERE
+				MS2.stadium_name = MS.stadium_name
+			GROUP BY
+				C2.username) AS Subquery)
+	ORDER BY
+		MS.stadium_name,
+		directed_count DESC;
 
--- Query 15
--- Find the jury who has rated the highest number of match sessions in the database. The required column names are respectively: name, surname, rated sessions.;
+	
 
--- Query 16
--- Find the average rating of match sessions for each coach, and list the coaches in descending order of their average ratings. The required column names are respectively: name, surname, average rating. Sort by name in descending order.;
+-- ✅ Query 14
+-- List all the matches that were played after 2023 (meaning starting from 01.01.2024 inclusive), and have not been directed by “Ferhat Akbas”. The required column names are respectively: session ID, name, surname. Sort by session id in ascending order.
+;
+SELECT M.session_ID, C.name, C.surname
+FROM MatchSession M
+JOIN Team T ON T.team_ID = M.team_ID
+JOIN Coach C ON C.username = T.coach_username
+WHERE STR_TO_DATE(M.`date`, '%d.%m.%Y') >= date('2024-01-01') AND C.username != 'f_akbas'
+ORDER BY M.session_ID;
+
+-- ✅ Query 15
+-- Find the jury who has rated the highest number of match sessions in the database. The required column names are respectively: name, surname, rated sessions.
+;
+SELECT
+	J.name,
+	J.surname,
+	COUNT(*) AS rated_sessions
+FROM
+	MatchSession MS
+	JOIN Jury J ON J.username = MS.assigned_jury_username
+GROUP BY
+	MS.assigned_jury_username
+HAVING
+	rated_sessions = (
+		SELECT
+			COUNT(*) AS rated_sessions
+		FROM
+			MatchSession MS
+			JOIN Jury J ON J.username = MS.assigned_jury_username
+		GROUP BY
+			MS.assigned_jury_username
+		ORDER BY
+			rated_sessions DESC
+		LIMIT 1);
+	
+-- ✅ Query 16
+-- Find the average rating of match sessions for each coach, and list the coaches in descending order of their average ratings. The required column names are respectively: name, surname, average_rating. Sort by name in descending order.
+;
+SELECT C.username, AVG(MS.rating) as average_rating FROM
+MatchSession MS
+JOIN Team T ON T.team_ID = MS.team_ID
+JOIN Coach C ON C.username = T.coach_username
+GROUP BY C.username
+ORDER BY average_rating DESC;
 
 -- Query 17
--- For each team name, find the coach who has signed a contract that has the longest period with that team, and has never directed a match with a rating lower than 4.7 (4.7 is not acceptable) with ANY team. Please note that if there are no coaches who meet these conditions, show the coach name, surname, and day count as NULL or None. The required column names are respectively: team name, name, surname, day count.;
+-- For each team name, find the coach who has signed a contract that has the longest period with that team, and has never directed a match with a rating lower than 4.7 (4.7 is not acceptable) with ANY team. Please note that if there are no coaches who meet these conditions, show the coach name, surname, and day count as NULL or None. The required column names are respectively: team_name, name, surname, day count.
+;
 
--- Query 18
--- Find the names of coaches who have directed at least one match in each stadium. The required column names are respectively: name, surname, played count.;
+-- ✅ Query 18
+-- Find the names of coaches who have directed at least one match in each stadium. The required column names are respectively: name, surname, played_count.
+;
+SELECT
+	C.name,
+	C.surname,
+	COUNT(DISTINCT MS.stadium_name) AS played_count
+FROM
+	Coach C
+	LEFT JOIN Team T ON T.coach_username = C.username
+	LEFT JOIN MatchSession MS ON MS.team_ID = T.team_ID
+GROUP BY
+	C.username
+HAVING
+	played_count = (
+		SELECT
+			COUNT(DISTINCT stadium_name)
+		FROM
+			MatchSession MS
+		WHERE
+			MS.stadium_name IS NOT NULL);
 
--- Query 19
--- Return the players who have played in stadium name “GD Voleybol Arena” as position name “Libero”. The required column names are respectively: name, surname.;
+-- ✅ Query 19
+-- Return the players who have played in stadium name “GD Voleybol Arena” as position name “Libero”. The required column names are respectively: name, surname.
+;
+SELECT DISTINCT P.name, P.surname
+FROM MatchSession MS
+JOIN Team T ON T.team_ID = MS.team_ID
+JOIN PlayerTeams PT ON PT.team = T.team_ID
+JOIN Player P ON P.username = PT.username
+JOIN PlayerPositions PP ON PP.username = P.username
+WHERE PP.`position` = 'Libero' AND MS.stadium_name = 'GD Voleybol Arena';
 
--- Query 20
--- List all player ID’s with the column more than one which is either TRUE if that player plays more than one position or FALSE otherwise. The required column names are respectively: name, surname, more than one. Sort in ascending order by name.;
+-- ✅ Query 20
+-- List all player ID’s with the column more than one which is either TRUE if that player plays more than one position or FALSE otherwise. The required column names are respectively: name, surname, more than one. Sort in ascending order by name.
+;
 SELECT
 	name,
 	surname,
